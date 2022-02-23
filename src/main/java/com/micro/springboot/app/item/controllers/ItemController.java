aad7b0c0654740b682e4655520192f8c5f6a0d9a
Bryam Castañeda <bryamdev.contact@gmail.com>
2022-02-22 22:28:18 -0500

    Se copio el metodo verItem() y su fallback para agregar anotaciones de CircuitBraker y TimeLimiter, finalizada seccion Resilience4J

--- src/main/java/com/micro/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/micro/springboot/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package com.micro.springboot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.micro.springboot.app.item.models.Item;
 import com.micro.springboot.app.item.models.Producto;
 import com.micro.springboot.app.item.models.service.ItemService;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -49,6 +55,23 @@ public class ItemController {
 				.run( ()-> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
 	}
 	
+	//Detalle pero version anotada con CircuitBraker para crear el circuito
+	//Con la anotacion solo se toma la configuracion hecha por archivo (properties o yml) no la hecha por beans
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
+	public Item verItem2(@PathVariable Long id, @PathVariable Integer cantidad){
+		return itemService.findById(id, cantidad);
+	}
+	
+	//Se debe envolver en una llamada futura asincrona 'CompletableFuture' para calcular tiempo de time out
+	@Bulkhead(name = "items", type = Type.THREADPOOL)
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name = "items")
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item>  verItem3(@PathVariable Long id, @PathVariable Integer cantidad){
+		return CompletableFuture.supplyAsync( () -> itemService.findById(id, cantidad));
+	}
+	
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e){
 		
 		log.info("Error: " + e.getMessage());
@@ -63,8 +86,25 @@ public class ItemController {
 		
 		item.setProducto(producto);
 		
-		return item;
+		return item;		
+	}
+	
+	//Metodo alternativo con representacin futura para el metodo verItem3()
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e){
 		
+		log.info("Error: " + e.getMessage());
+		
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Salvavidas");
+		producto.setPrecio(11.11);
+		
+		item.setProducto(producto);
+		
+		return CompletableFuture.supplyAsync( () -> item );		
 	}
 	
 }
